9004cb832d0081f8d77f658b2732377c4cfb154d
Mathew Bramson <mathewbramson@gmail.com>
2017-12-17 23:46:16 -0500

    Add last_feeling to Feelings show action

--- lib/feelingsmotron_web/controllers/feelings_controller.ex
+++ lib/feelingsmotron_web/controllers/feelings_controller.ex
@@ -6,15 +6,18 @@ defmodule FeelingsmotronWeb.FeelingsController do
   alias Feelingsmotron.Feelings
 
   def show(conn, _params) do
+    current_user = Guardian.Plug.current_resource(conn)
+    last_feeling = Feelings.last_feeling(current_user)
+
     conn
-    |> render("show.json", feelings: 1)
+    |> render("show.json", feelings: last_feeling)
   end
 
   def create(conn, %{"feelings" => feelings}) do
     current_user = Guardian.Plug.current_resource(conn)
     attributes = %{value: feelings, user_id: current_user.id}
 
-    with {:ok, changeset} <- Feelings.create_feeling(attributes) do
+    with {:ok, _changeset} <- Feelings.create_feeling(attributes) do
       conn
       |> put_status(:created)
       |> render("show.json", feelings: feelings)
--- lib/feelingsmotron_web/views/feelings_view.ex
+++ lib/feelingsmotron_web/views/feelings_view.ex
@@ -1,7 +1,14 @@
 defmodule FeelingsmotronWeb.FeelingsView do
   use FeelingsmotronWeb, :view
-  alias FeelingsmotronWeb.SessionView
 
+  alias Feelingsmotron.Feelings.Feeling
+
+  def render("show.json", %{feelings: %Feeling{value: value}}) do
+    %{feelings: value}
+  end
+  def render("show.json", %{feelings: nil}) do
+    %{feelings: nil}
+  end
   def render("show.json", %{feelings: feelings}) do
     %{feelings: feelings}
   end
