bd5577730bbe03f684eb8796cb034b638f8579e8
Mathew Bramson <mathewbramson@gmail.com>
2017-12-31 14:38:37 -0500

    Add Groups.delete_group/2

--- lib/feelingsmotron/groups/groups.ex
+++ lib/feelingsmotron/groups/groups.ex
@@ -10,6 +10,8 @@ defmodule Feelingsmotron.Groups do
   alias Feelingsmotron.Groups.UserGroup
   alias Feelingsmotron.Account.User
 
+  def get_group(id), do: Repo.get(Group, id)
+
   @spec list_groups() :: [Types.group]
   def list_groups do
     Repo.all(Group)
@@ -22,4 +24,22 @@ defmodule Feelingsmotron.Groups do
     |> Group.changeset(%{name: name, owner_id: owner_id})
     |> Repo.insert
   end
+
+  @spec delete_group(Types.group | integer(), Types.user | integer()) :: {:ok, Types.group} | {:error, :forbidden | :not_found}
+  def delete_group(group_id, deleting_user) when is_integer(group_id) do
+    delete_group(get_group(group_id), deleting_user)
+  end
+  def delete_group(%Group{} = group, deleting_user_id) when is_integer(deleting_user_id) do
+    delete_group(group, Feelingsmotron.Account.get_user(deleting_user_id))
+  end
+  def delete_group(%Group{} = group, %User{} = deleting_user) do
+    cond do
+      group.owner_id != deleting_user.id ->
+        {:error, :forbidden}
+      true ->
+        Repo.delete(group)
+    end
+  end
+  def delete_group(nil, _user), do: {:error, :not_found}
+  def delete_group(_group, nil), do: {:error, :not_found}
 end
--- test/feelingsmotron/groups/groups_test.exs
+++ test/feelingsmotron/groups/groups_test.exs
@@ -2,6 +2,19 @@ defmodule Feelingsmotron.GroupsTest do
   use Feelingsmotron.DataCase
 
   alias Feelingsmotron.Groups
+  alias Feelingsmotron.Groups.Group
+
+  describe "get_group/1" do
+    test "returns nil when the group does not exist" do
+      assert Groups.get_group(999) == nil
+    end
+
+    test "returns the group if it does exist" do
+      group = insert(:group)
+      returned_group = Groups.get_group(group.id)
+      assert returned_group.id == group.id
+    end
+  end
 
   describe "list_groups/0" do
     test "lists all existing groups" do
@@ -16,16 +29,41 @@ defmodule Feelingsmotron.GroupsTest do
   end
 
   describe "create_group/2" do
-    test "fails if the associated user does not exist" do
-      assert {:error, _} = Groups.create_group("group_name", 999)
-    end
-
     test "creates a group with the correct owner" do
       user = insert(:user)
       assert {:ok, group} = Groups.create_group("group_name", user)
       assert group.owner_id == user.id
       assert group.name == "group_name"
       assert Repo.all(Groups.Group) |> length == 1
+    end
+
+    test "fails if the associated user does not exist" do
+      assert {:error, _} = Groups.create_group("group_name", 999)
+    end
+  end
+
+  describe "delete_group/2" do
+    test "succeeds when the deleting user is the group owner" do
+      owner = insert(:user)
+      group = insert(:group, %{owner: owner})
+      assert {:ok, group} = Groups.delete_group(group, owner)   
+      assert %Group{} = group
+    end
+
+    test "fails if the deleting user is not the group owner" do
+      not_owner = insert(:user)
+      group = insert(:group)
+      assert {:error, :forbidden} = Groups.delete_group(group, not_owner)   
+    end
+
+    test "fails if the deleting user does not exist" do
+      group = insert(:group)
+      assert {:error, :not_found} = Groups.delete_group(group, 999)   
+    end
+
+    test "fails if the group does not exist" do
+      user = insert(:group)
+      assert {:error, :not_found} = Groups.delete_group(999, user)   
     end
   end
 end
