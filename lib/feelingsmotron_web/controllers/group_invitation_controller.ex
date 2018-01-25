9e8cc8284c7ce0ef85fea1d835270b031edb863c
Mathew Bramson <mathewbramson@gmail.com>
2018-01-24 20:56:14 -0500

    Allow boolean from_group for controller

--- lib/feelingsmotron_web/controllers/group_invitation_controller.ex
+++ lib/feelingsmotron_web/controllers/group_invitation_controller.ex
@@ -17,6 +17,16 @@ defmodule FeelingsmotronWeb.GroupInvitationController do
 
   def create(conn, %{"group_invitation" =>
     %{"user_id" => user_id, "group_id" => group_id, "from_group" => "true"}}) do
+    create(conn, %{"group_invitation" =>
+      %{"user_id" => user_id, "group_id" => group_id, "from_group" => true}})
+  end
+  def create(conn, %{"group_invitation" =>
+    %{"user_id" => user_id, "group_id" => group_id, "from_group" => "false"}}) do
+    create(conn, %{"group_invitation" =>
+      %{"user_id" => user_id, "group_id" => group_id, "from_group" => false}})
+  end
+  def create(conn, %{"group_invitation" =>
+    %{"user_id" => user_id, "group_id" => group_id, "from_group" => true}}) do
 
     current_user = Guardian.Plug.current_resource(conn)
     with :ok <- Groups.user_can_manage_group_membership(current_user.id, group_id),
@@ -27,7 +37,7 @@ defmodule FeelingsmotronWeb.GroupInvitationController do
     end
   end
   def create(conn, %{"group_invitation" =>
-    %{"user_id" => user_id, "group_id" => group_id, "from_group" => "false"}}) do
+    %{"user_id" => user_id, "group_id" => group_id, "from_group" => false}}) do
 
     current_user = Guardian.Plug.current_resource(conn)
     with :ok <- validate_same_user(current_user.id, user_id),
@@ -37,7 +47,9 @@ defmodule FeelingsmotronWeb.GroupInvitationController do
       |> render("show.json", group_invitation: invitation)
     end
   end
-  def create(_conn, _params), do: {:error, :bad_request}
+  def create(_conn, params) do
+    {:error, :bad_request}
+  end
 
   @spec validate_same_user(any(), any()) :: :ok | {:error, :forbidden | :not_found}
   defp validate_same_user(same_id, same_id) when is_integer(same_id), do: :ok
